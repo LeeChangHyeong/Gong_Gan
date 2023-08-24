b518c74e2f189dbd6ee523b9e0436e4a3399bad0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-08-24 16:21:44 +0900

    [Feat] #1 MainViewController에 NavigationController를 설정하고 MainView를 MainViewController에 띄우게 설정

--- Gong_Gan/Gong_Gan/Controllers/MainViewController.swift
+++ Gong_Gan/Gong_Gan/Controllers/MainViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    override func loadView() {
+        view = MainView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
--- Gong_Gan/Gong_Gan/Views/MainView.swift
+++ Gong_Gan/Gong_Gan/Views/MainView.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class MainView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        backgroundColor = .white
+    }
 }
