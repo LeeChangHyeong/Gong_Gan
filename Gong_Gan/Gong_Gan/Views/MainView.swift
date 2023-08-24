75df1969c3c204b2e43b9d92fda1d6299d76d6dd
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-08-24 16:57:03 +0900

    [Feat] #1 background에 사진 등록 후 화면 크기에 맞게 조정

--- Gong_Gan/Gong_Gan/Views/MainView.swift
+++ Gong_Gan/Gong_Gan/Views/MainView.swift
@@ -8,18 +8,26 @@
 import UIKit
 
 class MainView: UIView {
+    let backGroundView: UIImageView = {
+        let view = UIImageView(frame: UIScreen.main.bounds)
+        view.image = UIImage(named: "1")!
+        view.contentMode =  UIView.ContentMode.scaleAspectFill
+        
+        return view
+    }()
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        addSubview(backGroundView)
         
-        setup()
-    
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setup() {
-        backgroundColor = .white
-    }
+    
 }
