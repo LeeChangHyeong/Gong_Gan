6d7ccfa487c7f223d78acac13773777960ff39cc
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-08-24 17:15:31 +0900

    [Feat] #1 background 사진 변경, topBar 와 bottomBar 생성 후 오토레이아웃 적용

--- Gong_Gan/Gong_Gan/Views/MainView.swift
+++ Gong_Gan/Gong_Gan/Views/MainView.swift
@@ -16,17 +16,65 @@ class MainView: UIView {
         return view
     }()
     
+    let topBarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        
+        return view
+    }()
+    
+    let bottomBarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        
+        return view
+    }()
+    
     
     
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        addSubview(backGroundView)
-        
+        addViews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addViews() {
+        addSubview(backGroundView)
+        backGroundView.addSubview(topBarView)
+        backGroundView.addSubview(bottomBarView)
+    }
+    
+    
+    private func setConstraints() {
+        topBarViewConstraints()
+        bottomBarViewConstraints()
+    }
+    
+    private func topBarViewConstraints() {
+        topBarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topBarView.topAnchor.constraint(equalTo: backGroundView.topAnchor, constant: 0),
+            topBarView.leadingAnchor.constraint(equalTo: backGroundView.leadingAnchor, constant: 0),
+            topBarView.trailingAnchor.constraint(equalTo: backGroundView.trailingAnchor, constant: 0),
+            topBarView.heightAnchor.constraint(equalToConstant: 127)
+        ])
+    }
+    
+    private func bottomBarViewConstraints() {
+        bottomBarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bottomBarView.bottomAnchor.constraint(equalTo: backGroundView.bottomAnchor, constant: 0),
+            bottomBarView.leadingAnchor.constraint(equalTo: backGroundView.leadingAnchor, constant: 0),
+            bottomBarView.trailingAnchor.constraint(equalTo: backGroundView.trailingAnchor, constant: 0),
+            bottomBarView.heightAnchor.constraint(equalToConstant: 166)
+        ])
     }
     
     
