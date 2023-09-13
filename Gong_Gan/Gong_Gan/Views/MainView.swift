6864de16039bcffc74420d053a0e1cd06aee9470
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-09-13 23:44:47 +0900

    [Feat] #1 bottomView 안에 버튼UI 구현

--- Gong_Gan/Gong_Gan/Views/MainView.swift
+++ Gong_Gan/Gong_Gan/Views/MainView.swift
@@ -18,7 +18,7 @@ class MainView: UIView {
     lazy var backGroundView: UIImageView = {
         let view = UIImageView(frame: UIScreen.main.bounds)
         view.image = UIImage(named: "1")!
-        view.contentMode =  UIView.ContentMode.scaleAspectFill
+        view.contentMode = UIView.ContentMode.scaleAspectFill
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(backGroundViewTapped)))
         view.isUserInteractionEnabled = true
 
@@ -39,7 +39,46 @@ class MainView: UIView {
         return view
     }()
     
+    let galleryButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .buttonColor
+        button.setImage(UIImage(systemName: "book.closed.fill"), for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 3
+        
+        return button
+    }()
     
+    let addMemoButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .buttonColor
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 30
+        
+        return button
+    }()
+    
+    private let containerView: UIView = {
+      let view = UIView()
+      view.backgroundColor = .clear
+      view.layer.borderWidth = 3
+      view.layer.cornerRadius = 35
+      view.layer.borderColor = UIColor.buttonColor.cgColor
+      
+      return view
+    }()
+    
+    let settingButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .buttonColor
+        button.setImage(UIImage(systemName: "gearshape.fill"), for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 3
+        
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -56,13 +95,20 @@ class MainView: UIView {
         addSubview(backGroundView)
         backGroundView.addSubview(topBarView)
         backGroundView.addSubview(bottomBarView)
+        bottomBarView.addSubview(galleryButton)
+        bottomBarView.addSubview(containerView)
+        containerView.addSubview(addMemoButton)
+        bottomBarView.addSubview(settingButton)
         
     }
-    
     
     private func setConstraints() {
         topBarViewConstraints()
         bottomBarViewConstraints()
+        galleryButtonConstraints()
+        containerViewConstraints()
+        addMemoButtonConstratints()
+        settingButtonConstraints()
     }
     
     private func topBarViewConstraints() {
@@ -84,6 +130,51 @@ class MainView: UIView {
             bottomBarView.leadingAnchor.constraint(equalTo: backGroundView.leadingAnchor, constant: 0),
             bottomBarView.trailingAnchor.constraint(equalTo: backGroundView.trailingAnchor, constant: 0),
             bottomBarView.heightAnchor.constraint(equalToConstant: 166)
+        ])
+    }
+    
+    private func galleryButtonConstraints() {
+        galleryButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            galleryButton.leadingAnchor.constraint(equalTo: bottomBarView.leadingAnchor, constant: 50),
+            galleryButton.centerYAnchor.constraint(equalTo: bottomBarView.centerYAnchor),
+            galleryButton.widthAnchor.constraint(equalToConstant: 49),
+            galleryButton.heightAnchor.constraint(equalToConstant: 49)
+        ])
+    }
+    
+    
+    private func containerViewConstraints() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.centerXAnchor.constraint(equalTo: bottomBarView.centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: bottomBarView.centerYAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 70),
+            containerView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+    }
+    
+    private func addMemoButtonConstratints() {
+        addMemoButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            addMemoButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            addMemoButton.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            addMemoButton.widthAnchor.constraint(equalToConstant: 60),
+            addMemoButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    private func settingButtonConstraints() {
+        settingButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingButton.trailingAnchor.constraint(equalTo: bottomBarView.trailingAnchor, constant: -50),
+            settingButton.centerYAnchor.constraint(equalTo: bottomBarView.centerYAnchor),
+            settingButton.widthAnchor.constraint(equalToConstant: 49),
+            settingButton.heightAnchor.constraint(equalToConstant: 49)
         ])
     }
     
