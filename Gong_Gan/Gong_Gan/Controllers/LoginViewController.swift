29fe6ad06c6e1158971214a46846048175ed1d00
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-11-07 21:09:05 +0900

    LoginViewController 생성

---
+++ Gong_Gan/Gong_Gan/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Gong_Gan
+//
+//  Created by 이창형 on 11/7/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
