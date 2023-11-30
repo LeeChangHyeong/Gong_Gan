baf7591cd8bde70516f93f4cbf05005999c9d766
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-11-30 13:08:17 +0900

    firebase 모든 데이터를 들고올 model 생성

---
+++ Gong_Gan/Gong_Gan/Pages/Gallery/Model/GalleryDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryDataModel.swift
+//  Gong_Gan
+//
+//  Created by 이창형 on 11/30/23.
+//
+
+import Foundation
+
+struct GalleryDataModel {
+    let date: String
+    let imageName: String
+    let location: String
+    let memo: String
+    let time: String
+}
