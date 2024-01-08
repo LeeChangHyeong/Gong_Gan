d65c331fefd2620fa645011084c767d6cbde457d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-01-08 13:49:05 +0900

    온보딩 수정, 런치페이지 구현

--- Gong_Gan/Gong_Gan/AppDelegate.swift
+++ Gong_Gan/Gong_Gan/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         // 카카오 키 초기화
         KakaoSDK.initSDK(appKey: Keys.kakaoKey)
         
--- Gong_Gan/Gong_Gan/Pages/OnBoarding/OnBoardingViewController.swift
+++ Gong_Gan/Gong_Gan/Pages/OnBoarding/OnBoardingViewController.swift
@@ -50,14 +50,14 @@ class OnBoardingViewController: UIPageViewController {
     
     
     private func setupPage() {
-        let page1 =  OnBoardingPageViewController(imageName: "지하철",
+        let page1 =  OnBoardingPageViewController(imageName: "온보딩1",
                                                   title: "오늘은 어떤 순간을\n찍어볼까요?",
                                                   subTitle: "셔터를 눌러 사진을 찍고, 자유롭게 기록해보세요.")
-        let page2 = OnBoardingPageViewController(imageName: "지하철",
-                                                 title: "온전히 집중할 수 있는\n나만의 공간",
-                                                 subTitle: "날씨와 시간에 따라 달라지는 풍경을 만나보세요")
-        let page3 = OnBoardingPageViewController(imageName: "지하철",
-                                                 title: "변하지 않는\n그날의 순간들",
+        let page2 = OnBoardingPageViewController(imageName: "온보딩2",
+                                                 title: "밖에 비가오면\n나만의 공간",
+                                                 subTitle: "날씨와 시간에 따라 달라지는 풍경을 만나보세요.")
+        let page3 = OnBoardingPageViewController(imageName: "온보딩3",
+                                                 title: "변하지 않는\n그날의 기록들",
                                                  subTitle: "나의 기록을 한 눈에 볼 수 있어요.")
         pages.append(page1)
         pages.append(page2)
